632b5999988b37c4af780e9e67b49496ab887d4d
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-09-24 12:31:17 +0700

    Song Card Component

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,18 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
+
+const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
+  isPlaying && activeSong?.title === song.title ? (
+    <FaPauseCircle
+      size={35}
+      onClick={handlePause}
+      className=" text-gray-300 cursor-pointer"
+    />
+  ) : (
+    <FaPlayCircle
+      size={35}
+      onClick={handlePlay}
+      className=" text-gray-300 cursor-pointer"
+    />
+  );
 
 export default PlayPause;
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -2,10 +2,13 @@
 import React from "react";
 import { Error, Loader, SongCard } from "../components";
 import { genres } from "../assets/constants";
+import { useDispatch, useSelector } from "react-redux";
 
 import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 
 const Discover = () => {
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
   // data: result of the api call
 
   const { data, isFetching, error } = useGetTopChartsQuery();
@@ -23,6 +26,7 @@ const Discover = () => {
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10 ">
         <h2 className="font-bold text-3xl text-white">Discover</h2>
         <select
+          onChange={() => {}}
           value=""
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
@@ -36,7 +40,14 @@ const Discover = () => {
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {data?.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard
+            key={song.key}
+            song={song}
+            i={i}
+            activeSong={activeSong}
+            isPlaying={isPlaying}
+            data={data}
+          />
         ))}
       </div>
     </div>
