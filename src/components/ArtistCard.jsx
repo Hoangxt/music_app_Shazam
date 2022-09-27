c439c94c207937380134ea83aecf4320361857ad
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-09-27 12:03:55 +0700

    top charts and top artist

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,5 +1,24 @@
-const ArtistCard = () => {
-  <div>ArtistCard</div>;
+import React from "react";
+import { useNavigate } from "react-router-dom";
+const ArtistCard = ({ track }) => {
+  //navigate allow to move different page
+  const navigate = useNavigate();
+
+  return (
+    <div
+      className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+      onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}
+    >
+      <img
+        src={track?.images?.coverart}
+        alt="artist"
+        className="w-full h-56 rounded-lg"
+      />
+      <p className="text-white font-semibold text-lg mt-4 truncate">
+        {track?.subtitle}
+      </p>
+    </div>
+  );
 };
 
 export default ArtistCard;
